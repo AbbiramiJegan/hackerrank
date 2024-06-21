96130604f1e721d5e6b01d9910d9b210869b1e20
AbbiramiJegan <108280861+AbbiramiJegan@users.noreply.github.com>
2024-06-21 18:23:09 +0800

    Changed file names to camel case convention

--- sum_array_elements.py
+++
@@ -1,9 +0,0 @@
-def simpleArraySum(arr):
-    sum = 0
-    for i in range (len(arr)):
-        sum = sum + arr[i]
-    return sum
-
-arr = [1, 2, 3, 4, 10, 11]
-result = simpleArraySum(arr)
-print(result)
